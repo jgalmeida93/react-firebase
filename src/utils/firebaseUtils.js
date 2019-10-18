ef2f1e190d328a0ecb391d056a8c4be71bfd179d
Jonas Gabriel <jgalmeida1993@gmail.com>
2019-10-18 18:39:41 -0300

    Update firebaseUtils.js

--- src/utils/firebaseUtils.js
+++ src/utils/firebaseUtils.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDutA7fCbavKdgVNB4d_019vvBjbWvif1M",
-    authDomain: "jbook-7589e.firebaseapp.com",
-    databaseURL: "https://jbook-7589e.firebaseio.com",
-    projectId: "jbook-7589e",
-    storageBucket: "jbook-7589e.appspot.com",
-    messagingSenderId: "133812540206",
-    appId: "1:133812540206:web:75806ee49beddc25da20bf",
-    measurementId: "G-GJKRQ59T3N"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
  // Initialize Firebase
  export const firebaseImpl = firebase.initializeApp(config);
